32a7baadffa6d7423cb270a2f5d10b42aaf69ecd
adamarArcane <golywoper@gmail.com>
2024-09-01 20:53:57 -0400

    added player rotation

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -17,3 +17,14 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen,(255,255,255),self.triangle(),2)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(0-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
