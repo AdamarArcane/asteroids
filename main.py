3486ac1495f4f2a26462710fee16d828ce602757
adamarArcane <golywoper@gmail.com>
2024-09-01 20:44:10 -0400

    added draw player

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
